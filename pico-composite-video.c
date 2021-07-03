3e5662bdf7466ecc945161cebbddd0a759c816c4
Alan Reed <alan_p_reed@hotmail.co.uk>
2021-07-03 10:28:28 +0100

    Created initial project files

---
+++ pico-composite-video.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/dma.h"
+#include "hardware/pio.h"
+#include "hardware/timer.h"
+#include "hardware/clocks.h"
+
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    // Put your timeout handler code in here
+    return 0;
+}
+
+
+
+int main()
+{
+    stdio_init_all();
+
+
+
+    // Timer example code - This example fires off the callback after 2000ms
+    add_alarm_in_ms(2000, alarm_callback, NULL, false);
+
+
+    puts("Hello, world!");
+
+    return 0;
+}
