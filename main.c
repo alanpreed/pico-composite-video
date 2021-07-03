3a9822fd2d874f16945f9dff62df322ea4cd696b
Alan Reed <alan_p_reed@hotmail.co.uk>
2021-07-03 11:17:29 +0100

    Renamed main, added serial output

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 #include "hardware/pio.h"
 #include "hardware/timer.h"
 #include "hardware/clocks.h"
+#include "hardware/uart.h"
+
+
 
 int64_t alarm_callback(alarm_id_t id, void *user_data) {
     // Put your timeout handler code in here
@@ -11,18 +14,18 @@ int64_t alarm_callback(alarm_id_t id, void *user_data) {
 }
 
 
-
 int main()
 {
+    // Defaults: UART 0, TX pin 0, RX pin 1, baud rate 115200
     stdio_init_all();
-
-
+    gpio_set_function(PICO_DEFAULT_UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(PICO_DEFAULT_UART_RX_PIN, GPIO_FUNC_UART);
 
     // Timer example code - This example fires off the callback after 2000ms
     add_alarm_in_ms(2000, alarm_callback, NULL, false);
 
-
     puts("Hello, world!");
+    printf("Test\r\n");
 
     return 0;
 }
